772564f7b7a31ef24262365386c9023017e8d859
dclechok <dclechok@gmail.com>
2022-09-14 19:40:34 -0400

    adjusting UI, adding inputs

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
         <h1>Welcome to Palmetto</h1>
         <h5>CSV Data Parser</h5>
       </header>
+      <hr />
       <div>
         <input type="file" name="new-csv" accept=".csv" />
+        <input type="file" name="master-csv" accept=".csv" />
       </div>
     </div>
   );
