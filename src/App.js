e536eeca5e4a0bf94bbd33805734c03875f51037
dclechok <dclechok@gmail.com>
2022-09-15 08:52:22 -0400

    building UI with inputs, and log

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ function App() {
     <div className="App">
       <header>
         <h1>Welcome to Palmetto</h1>
-        <h5>CSV Data Parser</h5>
+        <h5>CSV Data Parser/Merger</h5>
       </header>
       <hr />
-      <div>
+      <div className="main-display-container">
+        <div className="input-container">
+        <label>New Data</label><br />
         <input type="file" name="new-csv" accept=".csv" />
+        </div>
+        <div className="input-container">
+        <label>Master File</label><br />
         <input type="file" name="master-csv" accept=".csv" />
+        </div>
       </div>
+      <h4>[ Log ]</h4>
+      <section>
+        <p>Hello</p>
+      </section>
     </div>
   );
 }
